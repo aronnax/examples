e3f739cda638011384d9c828bad7756b9c787755
msecret <msegreto@miceover.com>
2015-07-14 22:44:51 -0700

    Updated with new collision detection

--- src/ping.js
+++ src/ping.js
@@ -37,34 +37,76 @@ function setupRender(selector) {
 
 var rendrr = setupRender('.htmlcontext');
 
+console.debug('collision', component.collision);
 var Ball = makeEntityProto({className: 'Ball'}, rendrr,
     component.bounded,
     component.moveable,
-    component.rectangular);
+    component.rounded,
+    component.collision);
+console.debug('Ball', Ball);
 
 var Pin = makeEntityProto({className: 'Pin'}, rendrr,
     component.moveable,
     component.rectangular,
     component.rounded);
 
-function main(window) {
-  var loop = Object.create(Looping);
+const pinps = [
+  {x: 510, y: 40},
+  {x: 20, y: 150},
+  {x: 220, y: 200},
+  {x: 400, y: 350},
+  {x: 140, y: 100}
+];
 
-  let ball = Ball.make();
-  ball.init({w: 10, h: 10, y: 20});
+function main(window) {
+  var loop = Object.create(Looping),
+      ball,
+      pins = [];
+
+  for (let i = 0, ilen = 5; i < ilen; i++) {
+    let pin = Pin.make();
+    pin.init({
+      x: pinps[i].x,
+      y: pinps[i].y,
+      r: 20
+    });
+    pin._element.style.border = '3px solid gray';
+    pins.push(pin);
+  }
+
+  ball = Ball.make();
+  ball.init({w: 10, h: 10, r: 5, y: 20});
   ball.v.x = 1;
   ball.v.y = 1;
+  ball._element.style.border = '1px solid gray';
+  console.debug('pin', pins[1]);
+  console.debug('ball', ball);
+  ball.on('collision', (data) => {
+    var currentBall = data.a,
+        collidedPin = data.b;
 
-  let pin = Pin.make();
-  pin.init({w: 15, h: 15, x: 50, y: 50, r: {br: 5, bl: 5, tr: 5, tl: 5}});
+    console.log('collision', collidedPin);
+  });
 
   loop.onConstantly(dt => {
     ball.update(dt);
-    pin.update(dt);
+    for (let pin of pins) {
+      pin.update(dt);
+      let isCol = ball.collision.checkCollision(ball, pin);
+      if (isCol) {
+        ball.bg = '#cc1212';
+        pin.bg = '#094ee2';
+      } else {
+        ball.bg = '#000';
+        pin.bg = '#000';
+      }
+    }
   });
   loop.onEveryFrame(dt => {
     ball.render();
-    pin.render();
+    for (let pin of pins) {
+      pin.render();
+    }
   });
   loop.start();
 
